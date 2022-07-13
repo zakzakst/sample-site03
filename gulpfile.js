1edfd863c4d8bbcbd2de09d2d29a964277615f18
zakzakst <e1433104@gmail.com>
2022-07-13 18:09:27 +0900

    nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const { task, watch, series } = require('gulp');
 /**
  * タスクモジュール読み込み
  */
-// const { NUNJUCKS_BUILD } = require('./tasks/nunjucks-build');
+const { NUNJUCKS_BUILD } = require('./tasks/nunjucks-build');
 const { SASS_BUILD } = require('./tasks/sass-build');
 // const { WEBPACK } = require('./tasks/webpack');
 // const { IMAGE_MIN } = require('./tasks/image-min');
@@ -23,7 +23,7 @@ const { SASS_BUILD } = require('./tasks/sass-build');
 // });
 // task('default', series(BROWSER_START, 'watchFiles'));
 // task('build', series(NUNJUCKS_BUILD, SASS_BUILD, WEBPACK));
-// task('nunjucksBuild', series(NUNJUCKS_BUILD));
+task('nunjucksBuild', series(NUNJUCKS_BUILD));
 task('sassBuild', series(SASS_BUILD));
 // task('webpack', series(WEBPACK));
 // task('imageMin', series(IMAGE_MIN));
---
+++ src/nunjucks/components/go-top.njk
@@ -0,0 +1,7 @@
+<!-- ▼▼▼ トップへ戻る ▼▼▼ -->
+<div id="js-go-top" class="go-top">
+  <button class="go-top__button">
+    <i class="fas fa-arrow-up"></i>
+  </button>
+</div>
+<!-- ▲▲▲ トップへ戻る ▲▲▲ -->
